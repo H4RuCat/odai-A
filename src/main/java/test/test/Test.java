dbd06f840be65d839b10cd4a3eafef0c70aa69a3
H4RuB4RE <kasyount@gmail.com>
2022-06-03 10:55:04 +0900

    にゃ～
    にゃにゃ！？！！？？！？

--- src/main/java/test/test/Test.java
+++ src/main/java/test/test/Test.java
@@ -7,7 +7,6 @@ public final class Test extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        super.onEnable();
 
         // event登録
         getServer().getPluginManager().registerEvents(new Listeners(), this);
@@ -16,7 +15,5 @@ public final class Test extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-
-        super.onDisable();
     }
 }
